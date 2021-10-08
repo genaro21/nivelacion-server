e7d7218fe2f410294071240de239cdaf278a0cc2
genaro21 <gallardo2147@gmail.com>
2021-10-08 21:16:30 +0200

    terminado

--- src/controllers/user.controllers.js
+++ src/controllers/user.controllers.js
@@ -3,12 +3,29 @@ const jwt = require("jsonwebtoken");
 const config = require("../config");
 
 const create = async (req, res) => {
-  try {
-    const user = await models.user.create(req.body);
-    return res.json({ user });
-  } catch (err) {
-    return res.json({ err });
+  // try {
+  // const user = await models.user.create(req.body);
+
+  // return res.json({ user });
+
+  //   } catch (err) {
+  //     return res.json({ err });
+  //   }
+  // };
+  const { email, password1, password2 } = req.body;
+  if (!email || !password1 || !password2) {
+    console.log("campos llenos por favor");
+    return res.json("Rellena todos los campos");
+  } else if (password1 !== password2) {
+    return res.json("Los password no coinciden");
   }
+  const user = {
+    email,
+    password: password1,
+  };
+  console.log({ user });
+  await models.user.create(user);
+  return res.json(req.body);
 };
 
 const all = async (req, res) => {
@@ -29,7 +46,7 @@ const all = async (req, res) => {
 const login = async (req, res) => {
   try {
     const user = await models.user.findOne({ email: req.body.email });
-    console.log("hola");
+
     if (!user) {
       return res.json({ error: "User no existe" });
     }
@@ -38,14 +55,26 @@ const login = async (req, res) => {
     }
     const token = jwt.sign({ user }, config.jwt.secret);
     console.log(token);
-    return res.json({ token, userId: user._id });
+    return res.json({
+      token: token,
+      userId: user._id,
+      admin: user.admin,
+    });
   } catch (err) {
     return res.json({ err });
   }
+};
+const remove = async (req, res) => {
+  const { id } = req.params;
+
+  const user = await models.user.findOneAndRemove({ _id: id });
+  res.json({ user });
+  return res.json("user deleted");
 };
 
 module.exports = {
   create,
   all,
   login,
+  remove,
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -5,7 +5,8 @@ const middleware = require("../middleware");
 const router = Router();
 
 router.post("/create", controllers.user.create);
-router.get("/all", controllers.user.all);
+router.get("/all", middleware.users.isValid, controllers.user.all);
 router.post("/login", controllers.user.login);
+router.delete("/remove/:id", controllers.user.remove);
 
 module.exports = router;
